cd601b8fefad8a765da5424fe0443d37999fe901
Isaac Lee <isaaclee@Isaacs-MacBook-Pro.local>
2025-12-02 20:04:42 -0500

    backend updates

--- stats-backend/stats/spotify.py
+++ stats-backend/stats/spotify.py
@@ -2,7 +2,36 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from typing import List, Dict, Any
 
-scope = 'user-library-read'
+def get_spotify_client():
+    
+    scope = (
+        'user-read-private'
+        'playlist-read-private'
+        'playlist-read-collaborative'
+        'user-library-read'
+    )
+    
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+def get_all_playlists(sp: spotipy.Spotify) -> List[Dict[str, Any]]: # need to wrap in List because spotify returns a JSON format
+    playlists = [] 
+    results = sp.current_user_playlists()
+    
+    while results:
+        playlists.extend(results['items'])
+        
+        if results['next']:
+            results = sp.next(results)
+        else:
+            break
+    
+    return playlists
+
+def get_playlists_tracks(sp: spotipy.Spotify, playlist_id: str) -> List[Dict[str, any]]:
+    tracks = []
+    results = sp.playlist
+
+
 
 
 
