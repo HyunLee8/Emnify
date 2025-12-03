6d60ec9fbe15e2144f2a5aff45d55183fec36cbf
Isaac Lee <isaaclee@Isaacs-MacBook-Pro.local>
2025-12-03 09:45:03 -0500

    playlist display changes

--- next-app/app/nav/insight/page.tsx
+++ next-app/app/nav/insight/page.tsx
@@ -2,10 +2,9 @@ import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 import UserPlaylist from "@/components/playlist-list";
 import ReturnDashboard from "@/components/return";
-import PlaylistSearch from "@/components/playlist-search";
 
 export default async function PlaylistInsights() {
-  // Check if user is authenticated
+
   const supabase = await createClient();
   const {
     data: { user },
@@ -21,7 +20,7 @@ export default async function PlaylistInsights() {
       <div className="mr-auto ml-30 justify-center items-center py-70">
         <h2 className="text-3xl mt-10">Playlist Insights</h2>
         <h1 className="text-6xl max-w-200 mt-3">Using a novelty Model to handle your playlist insights </h1>
-        <h2 className="font-bold text-2xl mt-6">Start review now ↓</h2>
+        <h2 className="font-bold text-2xl mt-5">Start review now ↓</h2>
       </div>
       <div className="mr-auto ml-10 mt-30">
         <UserPlaylist />
--- next-app/components/playlist-list.tsx
+++ next-app/components/playlist-list.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/lib/supabase/server";
 import { getSpotifyToken, formatUserPlaylists } from "@/lib/auth/spotify";
+import Link from "next/link";
 
 export default async function UserPlaylist() {
   const supabase = await createClient();
@@ -29,17 +30,24 @@ export default async function UserPlaylist() {
   }
 
   return (
-    <div className="w-full flex flex-col items-center justify-center gap-10">
-      <h1 className="mr-auto font-bold text-3xl">You playlists</h1>
-      <div className="grid grid-cols-1 gap-5">
+    <div className="w-full flex flex-col items-center p-5 bg-stone-950 rounded-xl h-screen">
+      <div className="flex w-full mb-3">
+        <h1 className="mr-auto font-bold text-xl">Your Library</h1>
+        <p className="text-4xl rounded-full">+</p>
+      </div>
+      <button className="mr-auto px-4 py-2 rounded-full bg-stone-700 mb-6">Playlists</button>
+      <div className="grid grid-cols-1 gap-2">
         {formattedPlaylists.map((pl) => (
-          <div key={pl.id} className="playlist-item flex items-center py-2 pl-4 pr-25 border border-white rounded-xl gap-20">
+          <div key={pl.id} className="playlist-item flex py-2 pr-25 rounded-xl gap-3">
             <img
               src={pl.image || ""}
               alt={`Cover image for ${pl.name}`}
               className="playlist-cover w-15 h-15 object-cover rounded-xl"
             />
-            <p className="playlist-name text-white font-bold">{pl.name}</p>
+            <div className="flex flex-col justify-center">
+              <p className="playlist-name text-white font-bold">{pl.name}</p>
+              <p>Playlist • {user.user_metadata.name}</p>
+            </div>
           </div>
         ))}
       </div>
